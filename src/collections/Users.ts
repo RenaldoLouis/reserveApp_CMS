c2b79244c93a1c496de050d0c4e8e5b5287345a2
RenaldoLouis <renaldolouis555@gmail.com>
2024-09-24 03:07:20 +0700

    try to remove the auth in users

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -2,10 +2,6 @@ import { CollectionConfig } from 'payload/types'
 
 const Users: CollectionConfig = {
   slug: 'users',
-  auth: true,
-  admin: {
-    useAsTitle: 'email',
-  },
   fields: [
     // Email added by default
     // Add more fields as needed
