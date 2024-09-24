5554f8f2db9a628258f57880bbea2c1ccad9017f
RenaldoLouis <renaldolouis555@gmail.com>
2024-09-24 09:52:15 +0700

    revert the auth in user is needed

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -2,6 +2,10 @@ import { CollectionConfig } from 'payload/types'
 
 const Users: CollectionConfig = {
   slug: 'users',
+  auth: true,
+  admin: {
+    useAsTitle: 'email',
+  },
   fields: [
     // Email added by default
     // Add more fields as needed
